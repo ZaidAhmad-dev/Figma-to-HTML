6b108f586df6602aef49affdc6b95c230d9d96ff
Zaid Ahmad <ZaidAhmad-dev@users.noreply.github.com>
2022-07-11 09:32:54 -0700

    added projects and awards section

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var bars = document.getElementsByClassName("bars")[0];
+window.addEventListener("load", function(){
+    var bars = document.getElementsByClassName("bars")[0];
 bars.addEventListener("click", () => {
     var navListMobile = document.getElementsByClassName("nav-list-mobile")[0];
     // navListMobile.style.display = "block";
@@ -12,4 +13,30 @@ bars.addEventListener("click", () => {
         navListMobile.style.display = "block"
         bars.classList.add("fa-xmark")
     }
+})
+
+$('.projects-slider').slick({
+    mobileFirst: true,
+    dots: false,
+    infinite: false,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-arrow-left-long' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-arrow-right-long' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 576.98,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }      
+      },
+      {
+        breakpoint: 991.98,
+        settings: "unslick"
+      }
+    ]
+  });
 })
